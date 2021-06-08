55252be0856e937e3cac72ce8013e310caa3d83b
Vituz <vtbattaglia@gmail.com>
2021-06-09 01:13:11 +0200

    Add Tv shows category

--- asssets/js/main.js
+++ asssets/js/main.js
@@ -4,22 +4,48 @@ const app = new Vue({
 
     data:{
         query: '',
-        url: 'https://api.themoviedb.org/3/search/movie',
+        url: 'https://api.themoviedb.org/3/search/',
         apiKey: '03b14c3f1711a74b228ce8ae961435d6',
         language: 'it-IT',
+        page: 1,
+        filmsList:null,
+        countryFlag: `https://www.countryflags.io/`,
+        flagTipe:'/shiny/64.png', 
+        category:'',
     },
 
     methods: {
 
         searchFilm(){
-            let fullUrl = `${this.url}?api_key=${this.apiKey}&language=${this.language}&query=${this.query}`
-            console.log(fullUrl);
-            axios.get(fullUrl)
-            .then(resp => {
-                console.log(resp.data);
-            }).catch(e => {
-                console.error(e);
-            });
+
+            if(this.category == 'movies'){
+                let fullUrl = `${this.url}movie?api_key=${this.apiKey}&language=${this.language}&query=${this.query}&page=${this.page}`;
+                console.log(fullUrl);
+                this.query = '';
+                return this.axiosCall(fullUrl);
+
+            } else if(this.category == 'tvShows'){
+                let fullUrl = `${this.url}tv?api_key=${this.apiKey}&language=${this.language}&page=${this.page}&query=${this.query}`;
+                this.query = '';
+                return this.axiosCall(fullUrl);
+            }
+
+            
+        },
+
+        axiosCall(url){
+            axios.get(url)
+                .then(resp => {
+                    console.log(resp.data);
+                    this.filmsList = resp.data.results;
+                    console.log(this.filmsList);
+                }).catch(e => {
+                    console.error(e);
+                });
+        },
+
+        clearList(){
+            this.filmsList = [];
         }
     },
 
